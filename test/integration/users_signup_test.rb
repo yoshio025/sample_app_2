728f99b2a917916278d4e8f3a44db08e2d87abf1
吉田幸洋 <yoshidayukihiro@yoshidayukihironoMacBook-Air.local>
2018-09-11 15:02:34 +0900

    Use SSL and the Puma webserver in puroduction

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -18,8 +18,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "valid signup infomation" do
     get signup_path
-    assert_difference "User.count", 1 do
-      post user_path, params:{user:{name: "Example User",
+    assert_difference 'User.count', 1 do
+      post users_path, params:{user:{name: "Example User",
                          email: "user@example.com",
                          password: "password",
                          password_confirmation: "password"}}
